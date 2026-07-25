9a4100142be0859d1987a50ed7887b934cd0be1b
franklinikenuo <franklin.ikenuo@gdi.com>
2026-07-25 19:59:06 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,7 @@
+// ============================================================
+// CONFIG
+// ============================================================
+const supabaseUrl = "https://cpbkdtcrimppsxlstlob.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNwYmtkdGNyaW1wcHN4bHN0bG9iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA5NDEzMTMsImV4cCI6MjA5NjUxNzMxM30.oWvz_eKGwP7Po0SfSCHDNStCJanpn-c-gqaOkAjCJMI";
 
+const client = supabase.createClient(supabaseUrl, supabaseKey);
