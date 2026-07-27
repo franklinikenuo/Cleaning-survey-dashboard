f54691e0dd81243808f9a8458ad5151fbbc23930
franklinikenuo <franklin.ikenuo@gdi.com>
2026-07-27 18:41:39 -0400

    Refactor Supabase configuration in config.js
    Updated Supabase configuration variables and added console log for connection confirmation.

--- config.js
+++ config.js
@@ -1,11 +1,24 @@
 // ============================================================
-// CONFIG
+// SUPABASE CONFIGURATION
 // ============================================================
-window.supabaseUrl = "https://cpbkdtcrimppsxlstlob.supabase.co";
-window.supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNwYmtkdGNyaW1wcHN4bHN0bG9iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA5NDEzMTMsImV4cCI6MjA5NjUxNzMxM30.oWvz_eKGwP7Po0SfSCHDNStCJanpn-c-gqaOkAjCJMI";
+
+window.supabaseUrl =
+"https://cpbkdtcrimppsxlstlob.supabase.co";
+
+
+window.supabaseKey =
+"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNwYmtkdGNyaW1wcHN4bHN0bG9iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA5NDEzMTMsImV4cCI6MjA5NjUxNzMxM30.oWvz_eKGwP7Po0SfSCHDNStCJanpn-c-gqaOkAjCJMI";
+
+
+// Create shared Supabase client
 
 window.client =
 supabase.createClient(
     window.supabaseUrl,
     window.supabaseKey
+);
+
+
+console.log(
+    "✅ Supabase connected"
 );
