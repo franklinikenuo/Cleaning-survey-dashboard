17ab9bd5e07a710e50e3d45935afcf41171d65f6
franklinikenuo <franklin.ikenuo@gdi.com>
2026-07-27 18:38:20 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,11 @@
 // ============================================================
 // CONFIG
 // ============================================================
-const supabaseUrl = "https://cpbkdtcrimppsxlstlob.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNwYmtkdGNyaW1wcHN4bHN0bG9iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA5NDEzMTMsImV4cCI6MjA5NjUxNzMxM30.oWvz_eKGwP7Po0SfSCHDNStCJanpn-c-gqaOkAjCJMI";
+window.supabaseUrl = "https://cpbkdtcrimppsxlstlob.supabase.co";
+window.supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNwYmtkdGNyaW1wcHN4bHN0bG9iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA5NDEzMTMsImV4cCI6MjA5NjUxNzMxM30.oWvz_eKGwP7Po0SfSCHDNStCJanpn-c-gqaOkAjCJMI";
 
-const client = supabase.createClient(supabaseUrl, supabaseKey);
+window.client =
+supabase.createClient(
+    window.supabaseUrl,
+    window.supabaseKey
+);
